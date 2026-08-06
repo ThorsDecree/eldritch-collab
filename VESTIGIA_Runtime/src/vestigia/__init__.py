85e8835aad593d723f8041d0d672797e9e7a8af3
Jeff Padget <162010461+ThorsDecree@users.noreply.github.com>
2026-08-05 21:19:28 -0500

    feat: install Attention Router v0.1

--- VESTIGIA_Runtime/src/vestigia/__init__.py
+++ VESTIGIA_Runtime/src/vestigia/__init__.py
@@ -6,3 +6,8 @@ from .sensory_apparatus import install_core as _install_core
 
 _install_core()
 del _install_core
+
+from .attention_apparatus import install_core as _install_attention_router
+
+_install_attention_router()
+del _install_attention_router
