4379713e211ebe55bdc228df3b9d3145b0d74cca
Miskatonic Systems <kowen9024@gmail.com>
2026-08-06 20:43:03 -0700

    route builtins through explicit bootstrap

--- VESTIGIA_Runtime/src/vestigia/__init__.py
+++ VESTIGIA_Runtime/src/vestigia/__init__.py
@@ -1,28 +1,8 @@
 """VESTIGIA portable continuity runtime."""
 
-__version__ = "0.7.0"
+__version__ = "0.8.0.dev0"
 
-from .sensory_apparatus import install_core as _install_core
+from .bootstrap import bootstrap_runtime as _bootstrap_runtime
 
-_install_core()
-del _install_core
-
-from .attention_apparatus import install_core as _install_attention_router
-
-_install_attention_router()
-del _install_attention_router
-
-from .attention_keyring import install_core as _install_attention_keyring
-
-_install_attention_keyring()
-del _install_attention_keyring
-
-from .image_drawer_continuation import install_core as _install_image_drawer_continuation
-
-_install_image_drawer_continuation()
-del _install_image_drawer_continuation
-
-from .workshop_sandbox import install_core as _install_workshop_sandbox
-
-_install_workshop_sandbox()
-del _install_workshop_sandbox
+_bootstrap_runtime()
+del _bootstrap_runtime
